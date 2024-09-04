f2b0a39670d21cb8c79b862cb28e226b4892ca20
Максим Федоров <m88.fedorov@gmail.com>
2024-09-04 23:25:27 +0300

    Добавьте файлы проекта.

---
+++ Homework-28.08.2024-Log-Migration-Provider/Migrations/20240904191438_UpdateProductWithOrderId.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homework_28._08._2024_Log_Migration_Provider.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductWithOrderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Products");
+        }
+    }
+}
---
+++ Homework-28.08.2024-Log-Migration-Provider/Program.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Homework_28._08._2024_Log_Migration_Provider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AppContext db = new AppContext())
+            {
+                Service service = new Service();
+                Order order = new Order { OrderDate = DateTime.Now };
+                service.AddOrder(order);
+
+                Product product1 = new Product { Name = "Молоко", Price = 45 };
+                Product product2 = new Product { Name = "Хлеб", Price = 25 };
+
+                service.AddProductToOrder(order.Id, product1);
+                service.AddProductToOrder(order.Id, product2);
+
+                var receiveOrder = service.GetOrder(order.Id);
+                if (receiveOrder != null)
+                {
+                    Console.WriteLine("Order ID: " + receiveOrder.Id + " Date: " + receiveOrder.OrderDate);
+                }
+
+                //service.RemoveOrder(order.Id);
+            }
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+
+    public class Service
+    {
+        public void AddOrder(Order order)
+        {
+            using (AppContext db = new AppContext())
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+            }
+
+        }
+
+        public void RemoveOrder(int orderId)
+        {
+            using (AppContext db = new AppContext())
+            {
+                var order = db.Orders.FirstOrDefault(o => o.Id == orderId);
+                if (order != null)
+                {
+                    var products = db.Products.Where(p => p.OrderId == orderId).ToList();
+                    db.Products.RemoveRange(products);
+
+                    db.Orders.Remove(order);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public Order? GetOrder(int orderId)
+        {
+            using (AppContext db = new AppContext())
+            {
+                var order = db.Orders.FirstOrDefault(o => o.Id == orderId);
+                return order;
+            }
+        }
+
+        public void AddProductToOrder(int orderId, Product product)
+        {
+            using (var db = new AppContext())
+            {
+                product.OrderId = orderId;
+
+                db.Products.Add(product);
+                db.SaveChanges();
+            }
+        }
+
+    }
+
+    public class AppContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-M496S5I;Database=Shop;Trusted_Connection=True; TrustServerCertificate=True");
+        }
+    }
+}
